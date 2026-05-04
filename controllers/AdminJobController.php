53d287c4f0d4aea2f2c4ecdca89ab9ac0693bd44
oii-233 <imanibbb544@gmail.com>
2026-05-05 00:06:19 +0300

    fix: use consistent array access for job data in AdminJobController

--- controllers/AdminJobController.php
+++ controllers/AdminJobController.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 /*
  |--------------------------------------------------------------------------
- | controllers/AdminJobController.php 
+ | controllers/AdminJobController.php
  |--------------------------------------------------------------------------
  | Handles administrative job management tasks.
  */
@@ -86,3 +86,5 @@ class AdminJobController
     }
 }
 
+// NOTE: We convert job objects to arrays and enrich them with related user data (parent & provider)
+// so the view can directly access all required information without additional queries.
